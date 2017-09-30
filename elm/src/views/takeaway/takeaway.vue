de138269f70f6e9d59dbdb0363bfb55184805bc5
xiaomi12138 <476823686@qq.com>
2017-09-30 19:42:31 +0800

    轮播

--- elm/src/views/takeaway/takeaway.vue
+++ elm/src/views/takeaway/takeaway.vue
@@ -2,13 +2,8 @@
   <div>
     <!-- 头部导航栏 高度0.8rem-->
     <top-bar :takeaway="{takeaway}" :address="{address}" :mine="{mine}"></top-bar>
-    <!-- 食品分类 -->
-    <div class="foodc">
-      <section v-for="item of foodData" :key="item.id">
-        <img :src="item.image_url" :alt="item.title">
-        <span>{{item.title}}</span>
-      </section>
-    </div>
+    <!-- 食品分类轮播图 -->
+    <food-Bar></food-Bar>
     <!-- 底部导航栏 -->
     <nav-bar></nav-bar>
   </div>
@@ -17,70 +12,27 @@
 <script>
 import navBar from '../../components/navBar.vue'
 import topBar from '../../components/topBar.vue'
-import axios from 'axios'
+import foodBar from '../../components/foodBar.vue'
 export default {
   components: {
     navBar,
-    topBar
+    topBar,
+    foodBar
   },
   data () {
     return {
       // 头部导航栏信息
       takeaway: true,
       address: '南岸区重庆邮电大学',
-      mine: false,
-      // 食品分类信息
-      foodData: ''
+      mine: false
     }
   },
   created () {
-    this.classification()
   },
   methods: {
-    classification () {
-      let url = '/api/v2/index_entry'
-      let _this = this
-      axios.get(url)
-      .then((response) => {
-        _this.foodData = response.data
-        console.dir('食品分类信息：')
-        console.log(_this.foodData)
-        // url处理
-        for (let i = 0; i < _this.foodData.length; i++) {
-          _this.foodData[i].image_url = 'https://fuss10.elemecdn.com' + _this.foodData[i].image_url
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
   }
 }
 </script>
 
 <style lang="scss" scpoed>
-  .foodc {
-    display:flex;
-    flex-wrap: wrap;
-    margin-top: 0.8rem;
-    section {
-      display: flex;
-      flex-direction: column;
-      width: 25%;
-      justify-content: center;
-      padding: 0.16rem 0;
-      img{
-        display:inline-flex;
-        width: 0.8rem;
-        height: 0.8rem;
-        margin: 0 auto;
-      }
-      span {
-        text-align: center;
-        font-size: 0.26rem;
-        margin-top: 0.4rem;
-        color: #666;
-      }
-    }
-  }
 </style>
